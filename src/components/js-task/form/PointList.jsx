8c0dad69f02a0a22b4a5315e1e6462d9e795f501
dimitriskot <mopo_kdi-web@mail.ru>
2018-11-22 20:58:49 +0300

    draft DnD points (bug fixed) form (js-T)

--- src/components/js-task/form/PointList.jsx
+++ src/components/js-task/form/PointList.jsx
@@ -6,8 +6,8 @@ const PointList = (props) => {
 	const { points, onDeleteButtonClick } = props;
 	return (
 		<ul className="map-form__list outline-1" >
-			{points.map((point) => (
-				<PointItem key={point.id} point={point} index={point.index} onDeleteButtonClick={onDeleteButtonClick.bind(this, point.id)} />
+			{points.map((point, i) => (
+				<PointItem key={point.id} point={point} index={i} onDeleteButtonClick={onDeleteButtonClick.bind(this, point.id)} />
 			))}
 		</ul>
 	)
